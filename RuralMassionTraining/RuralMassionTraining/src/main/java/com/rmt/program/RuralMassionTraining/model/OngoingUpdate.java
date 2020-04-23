b35683d762756b0c13c0dc2c9f466a9be20492ff
Chiranjibee <chiranjibeem@gmail.com>
2020-04-23 21:15:29 +0530

    commit
    commit

--- RuralMassionTraining/RuralMassionTraining/src/main/java/com/rmt/program/RuralMassionTraining/model/OngoingUpdate.java
+++ RuralMassionTraining/RuralMassionTraining/src/main/java/com/rmt/program/RuralMassionTraining/model/OngoingUpdate.java
@@ -2,7 +2,17 @@ package com.rmt.program.RuralMassionTraining.model;
 
 public class OngoingUpdate {
     private String partner;
-
+    private String district;
+    private String dateAndTime;
+    private String batchCommenced;
+    private String batchType;
+    private String enrolledCandidates;
+    private String batchesCompleted;
+    private String houseTagged;
+    private String plinthLevel;
+    private String lintelLevel;
+    private String roofLevel;
+    private String completedLevel;
 
     public String getPartner() {
         return partner;
@@ -10,5 +20,93 @@ public class OngoingUpdate {
 
     public void setPartner(String partner) {
         this.partner = partner;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getDateAndTime() {
+        return dateAndTime;
+    }
+
+    public void setDateAndTime(String dateAndTime) {
+        this.dateAndTime = dateAndTime;
+    }
+
+    public String getBatchCommenced() {
+        return batchCommenced;
+    }
+
+    public void setBatchCommenced(String batchCommenced) {
+        this.batchCommenced = batchCommenced;
+    }
+
+    public String getBatchType() {
+        return batchType;
+    }
+
+    public void setBatchType(String batchType) {
+        this.batchType = batchType;
+    }
+
+    public String getEnrolledCandidates() {
+        return enrolledCandidates;
+    }
+
+    public void setEnrolledCandidates(String enrolledCandidates) {
+        this.enrolledCandidates = enrolledCandidates;
+    }
+
+    public String getBatchesCompleted() {
+        return batchesCompleted;
+    }
+
+    public void setBatchesCompleted(String batchesCompleted) {
+        this.batchesCompleted = batchesCompleted;
+    }
+
+    public String getHouseTagged() {
+        return houseTagged;
+    }
+
+    public void setHouseTagged(String houseTagged) {
+        this.houseTagged = houseTagged;
+    }
+
+    public String getPlinthLevel() {
+        return plinthLevel;
+    }
+
+    public void setPlinthLevel(String plinthLevel) {
+        this.plinthLevel = plinthLevel;
+    }
+
+    public String getLintelLevel() {
+        return lintelLevel;
+    }
+
+    public void setLintelLevel(String lintelLevel) {
+        this.lintelLevel = lintelLevel;
+    }
+
+    public String getRoofLevel() {
+        return roofLevel;
+    }
+
+    public void setRoofLevel(String roofLevel) {
+        this.roofLevel = roofLevel;
+    }
+
+    public String getCompletedLevel() {
+        return completedLevel;
+    }
+
+    public void setCompletedLevel(String completedLevel) {
+        this.completedLevel = completedLevel;
     }
 }
